79b923a39ee23e8f1401de112f248cc2c0e606be
saroj adhikari <adhikarisaroj291@gmail.com>
2024-11-02 17:06:30 +0545

    fixed issue

--- backend/server.js
+++ backend/server.js
@@ -3,9 +3,9 @@ const connectDb = require("./api/utils/dbConfig");
 app.get("/test", (req, res) => {
   res.json("test passed");
 });
-
+const port = process.env.PORT || 2050;
 connectDb();
-app.listen(process.env.PORT, (err) => {
+app.listen(port (err) => {
   if (err) {
     console.error("error in server", err);
     return;
